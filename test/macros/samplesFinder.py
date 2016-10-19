c83e371a440d43afcb7acf1069477a6387130e7e
Orso <oiorio@cern.ch>
2016-10-19 10:55:49 +0200

    new trees finder

--- test/macros/samplesFinder.py
+++ test/macros/samplesFinder.py
@@ -1,15 +1,38 @@
 import os, commands
+import optparse 
 
+
+usage = 'usage: %prog -se storage -v failed -d 161012'
+parser = optparse.OptionParser(usage)
+#Specify the base parameters, assuming you manually put samples and paths in the list below:
+parser.add_option('-s', '--storageElement',      dest='se',     type='string',     default = 'storage01.lcg.cscs.ch:8443/srm/managerv2',    help='Remote storaga element to fetch including port and manager version, examples: srm://storage01.lcg.cscs.ch:8443/srm/managerv2, stormfe1.pi.infn.it:8444/srm/managerv2')
+parser.add_option('-d', '--date',      dest='date',     type='string',     default = '',   help='date of the job, in the crab format yymmdd_hhmmss, also accepts partial (e.g: 1610 for october 2016)')
+parser.add_option('-v',   '--veto',     dest='veto',    type='string',     default = 'failed',  help='String to veto while fetching')
+
+
+parser.add_option('-o', '--outputDir',   dest='outdir',  type='string',     default = './',      help='Directory to create the samples folder')
+
+parser.add_option('-V',   '--verbose',     dest='verbose', type='int', default=0,help="verbose level:0=none, 1=only final list,2= detail from all functions")
+####If you want to import the samples and paths from a python file, work in progress:
+####parser.add_option('-i', '--importSamples',   dest='import',  type='string', default = None)
+
+#If you want to get the samples from a .txt file:
+parser.add_option('-f', '--file',      dest='file',     type='string',     default = '',      help='File for samples fetching')
+parser.add_option('-p','--path',      dest='path',     type='string',     default = '/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/',    help='local path for the samples, if file name is  specified')
+
+
+
+parser.add_option('-n', '--dryrun',   dest='dryrun',  action='store_true', default=False)
+
+(opt, args) = parser.parse_args()
 
 ###This funcTion searches recursively for root files in a remote path, requesting the path contains the word "sampleToFind" and returning an output file list:
-def findRootFileInPath(ls_command,srm, initialPath,sampleToFind,extraString="",date="", veto = "failed"):
-       
+def findRootFileInPath(ls_command,srm, initialPath,sampleToFind,extraString="",date="", veto = "failed",verbose=False):
     ls_command_exec = ls_command+srm+initialPath
-    print "command is: "+ ls_command_exec
+    if verbose: print "command is: "+ ls_command_exec
     status,ls_la = commands.getstatusoutput( ls_command_exec )
     list = []
     list = ls_la.split(os.linesep)
-    
     outlist =[]
     fetchlist =[]
     pulledlist =[]
@@ -43,21 +66,23 @@ def findRootFileInPath(ls_command,srm, initialPath,sampleToFind,extraString="",d
 
     if(len(outlist)==0):
         for l in fetchlist:
+            if verbose: print " fetching now path " + l 
             pulledlist = findRootFileInPath(ls_command=ls_command,srm=srm, initialPath=l,sampleToFind=sampleToFind,extraString = extraString, date = date, veto = veto)
 
-            print " pulled list:"
-            print pulledlist
+            if verbose: print " pulled list:"
+            if verbose: print pulledlist
             for f in pulledlist:
                 outlist.append(f)
 
-    print "path is "+ initialPath
-    print " check outlist length "
-    print len(outlist)
+    if verbose:
+        print "path is "+ initialPath
+        print " check outlist length "
+        print len(outlist)
 #    if status: raise RFIOErr('Dir not found')
     return outlist
 
 ###This one copies a files in an input list to another directory, returning a list wiht the copied file names:
-def copyFilesInList(cp_command,srm, inputList, outputDir, maxSimultaneousCopies=1):
+def copyFilesInList(cp_command,srm, inputList, outputDir, maxSimultaneousCopies=1, verbose=False):
     
     m =0
     command_cp_exec = cp_command+srm
@@ -83,7 +108,7 @@ def copyFilesInList(cp_command,srm, inputList, outputDir, maxSimultaneousCopies=
             os.system(command_cp_exec)
 
 ###This one copies a files in an input list to another directory, returning a list wiht the copied file names:
-def printFilesInList(inputList, path, nameOut, outputDir, option = 'w'):
+def printFilesInList(inputList, path, nameOut, outputDir, option = 'w',verbose=False):
  
     filename = outputDir+"list"+nameOut+".py"
     if (option == 'a'):
@@ -97,25 +122,52 @@ def printFilesInList(inputList, path, nameOut, outputDir, option = 'w'):
     if(option == 'w'): f.write("filelist = [")
     for l in inputList:
         for lsplit in l.split(path):
-            print lsplit
+            if verbose: print lsplit
             if (".root" in lsplit): f.write("'root://xrootd.ba.infn.it//"+lsplit+"', \n")
     f.write("]\n")        
+
+def listFromSamples(f,p,verbose=False):
+    fopen = open(f,'r')
+    sp = {}
+#
+    for l in fopen.readlines():
+#        if verbose: print "reading line" +l
+#        print l 
+        parts = l.split("/")
+        if len(parts)<2: continue
+        sp[parts[1]] = p
+        if verbose: print sp
+    return sp
         
 ###MAIN###
 
 cmdls = "lcg-ls"
 cmdcp = "lcg-cp"
-srm = "  -b -D srmv2 srm://storage01.lcg.cscs.ch:8443/srm/managerv2\?SFN="
+#srmLocal = "  -b -D srmv2 srm://storage01.lcg.cscs.ch:8443/srm/managerv2\?SFN="
 
-#srm = " -b -D srmv2 srm://stormfe1.pi.infn.it:8444/srm/managerv2?SFN="
-
+srmLocal = "  -b -D srmv2 srm://"+ opt.se + "\?SFN="
 
 #samplesAndPaths = {"TTJets":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/decosa/ttDM/Phys14_Tree_v2/"}
+
 samplesAndPaths = {
-    "ST_t-channel":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "ST_t-channel_top":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "ST_t-channel_antitop":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "TT_Tune":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "ST_tW_top":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "ST_tW_antitop":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "ST_s-channel":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "WJetsToLNu":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
+    "DYJetsToLL":"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/oiorio/ttDM/samples/2016/Oct/",
     }
 
-def writeSamplesAndPaths(sap, outputDir="./", option = 'w'):
+
+if not opt.file == '':
+    print opt.file
+    samplesAndPaths = listFromSamples(opt.file, opt.path)
+
+outDir = opt.outdir
+
+def writeSamplesAndPaths(sap, outputDir=outDir, option = 'w'):
     filename = outputDir+"samplesAndPaths.py"
         
     f = open(filename, option)
@@ -126,25 +178,24 @@ def writeSamplesAndPaths(sap, outputDir="./", option = 'w'):
         f.write("'"+str(sample)+"':'"+str(sap[sample])+"',")
     f.write("}")  
   
-outDir = "/tmp/oiorio/"
+
 
 writeSamplesAndPaths(samplesAndPaths)
-os.system("mkdir samples")
-os.system("rm samples/listAllSamples.py")
-#os.system("rm jer/listAllSamples.py")
+os.system("mkdir "+outDir+"/samples")
+os.system("rm "+outDir+"/samples/listAllSamples.py")
 
 for sample in samplesAndPaths:
     path = samplesAndPaths[sample]
 
 #    outputList = []
     outputListJES = []
-    print path
-    outputListJES= findRootFileInPath(cmdls,srm,path,sample,extraString ="",date="", veto = "failed")
+    #print path
+    outputListJES= findRootFileInPath(cmdls,srmLocal,path,sample,extraString ="",date=opt.date, veto = opt.veto, verbose=(opt.verbose>1))
+    if (opt.verbose>0):
+        print "final output list for "+sample+" is : "
+        print outputListJES
 
-
-
-    print "final output list for "+sample+" is : "
-    print outputListJES
-
-    printFilesInList(outputListJES, "/pnfs/lcg.cscs.ch/cms/trivcat/", sample,"./samples/","w")
-    printFilesInList(outputListJES, "/pnfs/lcg.cscs.ch/cms/trivcat/", sample,"./samples/","a")
+    if not opt.dryrun:
+        printFilesInList(outputListJES, "/pnfs/lcg.cscs.ch/cms/trivcat/", sample,"./samples/","w",verbose=(opt.verbose>1))
+        printFilesInList(outputListJES, "/pnfs/lcg.cscs.ch/cms/trivcat/", sample,"./samples/","a",verbose=(opt.verbose>1))
+    
