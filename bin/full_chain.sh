2c0ededa3205169bba1d48370f225099b0f3a14e
wajidalikhan <wajid@cern.ch>
2017-01-10 13:52:23 +0100

    Removed outdated things from Build file

--- bin/full_chain.sh
+++ bin/full_chain.sh
@@ -7,16 +7,20 @@ echo "Renaming the xrd ..."
 python script_replacexrd.py -f ./files/renamed/ -o files/final -x xrootd.ba.infn.it -P ST,VV,VJ,TT,SingleMuon
 
 echo "Submitting jobs to cluster ... Remember to merge the output with the script:" 
-#python merge_res.py -l ./res/ -P ST,TT,VJ,VV --rm True
-#python merge_res.py -l ./res -P SingleMuon -c "muon,muonantiiso"
-#python merge_res.py -l ./res/ -P ST,TT,VJ,VV -c "muon,muonantiiso"
+python merge_res.py -l ./res/ -P ST,TT,VJ,VV --rm True
+
+python merge_res.py -l ./res -P SingleMuon -c "muon,muonantiiso"
+
+python merge_res.py -l ./res/ -P ST,TT,VJ,VV -c "muon,muonantiiso"
+
+python merge_res.py -l ./res/ -P _QCDMuEPt20toInf -c "muon,muonantiiso"
 
 
 #Running over the MC 
 python new_singletop.py --t3batch -f files/final/ -P ST,TT,VJ,VV -S 10
 
 #Running over muonantiiso
-python new_singletop.py -c muonantiiso -s noSys --t3batch -f files/final/ -P ST,TT,VJ,VV -S 10
+python new_singletop.py -c muonantiiso -s noSys --t3batch -f files/final/ -P ST,TT,VJ,VV,QCDMuEPt20toInf -S 10
 
 #Running over the data
 python new_singletop.py --t3batch -f files/final/ -P SingleMuon -d DATA -S 10
